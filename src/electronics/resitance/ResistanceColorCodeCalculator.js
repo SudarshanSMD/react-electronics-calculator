bc6d57bfa697819ffb23d9e9a7fe24aad420b416
Sudarshan Muralidhar Devardekar <sudarshansmd@gmail.com>
2019-09-10 16:14:33 +0530

    Code splitting for electronics, resitance

---
+++ src/electronics/resitance/ResistanceColorCodeCalculator.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class ResistanceColorCodeCalculator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div> This is Resistance colour code calculator.</div>
+        );
+    }
+}
+
+export default ResistanceColorCodeCalculator;
